40a165951b7df9e373bd8aa0f83df95fbca7a864
RagaBayuPurwanto <christianmercielago99@gmail.com>
2018-12-06 23:08:14 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+
+const router: Routes = [
+    { path: 'accomodation', component: AccomodationComponent },
+    { path: 'celebrations', component: CelebrationsComponent },
+    { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+    { path: 'lifestyle', component: LifestyleComponent },
+    { path: 'signin', component: SigninComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: '', component: HomeComponent }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(router)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
